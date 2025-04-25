8de906666c1a2fb9cb886b1267269d7e9234195d
“Nick <nickpantev33@gmail.com>
2025-04-25 20:35:14 +0300

    Header component created

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
-      <h1 className="text-white text-4xl font-bold">🌤️ Hello Tailwind!</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
